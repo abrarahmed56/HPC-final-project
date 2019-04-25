260242ab03e836d2775fa73d62215286ba5f1d96
Abrar Ahmed <abrarahmed257@gmail.com>
2019-04-25 14:27:18 -0400

    Fix pointer bug and calculate error

--- floyd-warshall.c
+++ floyd-warshall.c
@@ -4,49 +4,51 @@
 #include <math.h>
 #include <time.h>
 
-void floyd_warshall_serial(double* distance, int N) {
+double* makeCopy(double* original_array, int N) {
+  double* array_copy = (double*) malloc(N*N*sizeof(double));
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      *(array_copy+(i*N)+j) = *(original_array+(i*N)+j);
+    }
+  }
+  return array_copy;
+}
+
+double* floyd_warshall_serial(double* distance, int N) {
+  double* distanceCopy = makeCopy(distance, N);
   for (int k = 0; k < N; k++) {
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
-	if (*(distance+(i*N)+k) + *(distance+(k*N)+j) < *(distance+(i*N)+j)) {
-	  *(distance+(i*N)+j) = *(distance+(i*N)+k) + *(distance+(k*N)+j);
+	if (*(distanceCopy+(i*N)+k) + *(distanceCopy+(k*N)+j) < *(distanceCopy+(i*N)+j)) {
+	  *(distanceCopy+(i*N)+j) = *(distanceCopy+(i*N)+k) + *(distanceCopy+(k*N)+j);
 	}
       }
     }
   }
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      printf("%f, ", *(distance+(i*N)+j));
-    }
-    printf("\n");
-  }
+  return distanceCopy;
 }
 
-void floyd_warshall_omp(double* distance, int N) {
+double* floyd_warshall_omp(double* distance, int N) {
+  double* distanceCopy = makeCopy(distance, N);
   for (int k = 0; k < N; k++) {
 #pragma omp parallel
     {
     #pragma omp for
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
-	if (*(distance+(i*N)+k) + *(distance+(k*N)+j) < *(distance+(i*N)+j)) {
-	  *(distance+(i*N)+j) = *(distance+(i*N)+k) + *(distance+(k*N)+j);
+	if (*(distanceCopy+(i*N)+k) + *(distanceCopy+(k*N)+j) < *(distanceCopy+(i*N)+j)) {
+	  *(distanceCopy+(i*N)+j) = *(distanceCopy+(i*N)+k) + *(distanceCopy+(k*N)+j);
 	}
       }
     }
   }
   }
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      printf("%f, ", *(distance+(i*N)+j));
-    }
-    printf("\n");
-  }
+  return distanceCopy;
 }
 
 int main (int argc, char *argv[]) 
 {
-  int N = 4;
+  int N = 500;
   double* distance = (double*) malloc(N*N*sizeof(double));
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
@@ -63,13 +65,24 @@ int main (int argc, char *argv[])
   clock_t start, end;
   double cpu_time_used;
   start = clock();
-  floyd_warshall_serial(distance, N);
+  double* serial_matrix = floyd_warshall_serial(distance, N);
   end = clock();
   cpu_time_used = ((double) (end - start));
   printf("Time for serial code: %f\n", cpu_time_used);
   start = clock();
-  floyd_warshall_omp(distance, N);
+  double* parallel_matrix = floyd_warshall_omp(distance, N);
   end = clock();
   cpu_time_used = ((double) (end - start));
   printf("Time for omp code: %f\n", cpu_time_used);
+  double error = 0;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int index = i*N + j;
+      error += fabs(*(serial_matrix+index) - *(parallel_matrix+index));
+    }
+  }
+  printf("Error: %f\n", error);
+  free(distance);
+  free(serial_matrix);
+  free(parallel_matrix);
 }
